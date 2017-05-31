1725737204101d1b1efa5f062736f979a6e62988
Theo <theobaldo_medjunior@yahoo.com.br>
2017-05-30 22:44:25 -0300

    Class 11

---
+++ class_10.rb
@@ -0,0 +1,12 @@
+def sagan
+  
+  puts "Somewhere something incredible is waiting to be known."
+  
+end
+
+def osteen
+  puts "Fuck You Fucking Bastard"
+end
+
+sagan
+osteen
---
+++ class_11.rb
@@ -0,0 +1,5 @@
+array_test = ["first line", 'second_line', 'third_line']
+
+for item in array_test do
+  puts item
+end
