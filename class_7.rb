0e9f942442ae122df1b0d8b96cc01df879d33aaf
Theo <theobaldo_medjunior@yahoo.com.br>
2017-05-30 21:39:32 -0300

    Class 8

---
+++ class_7.rb
@@ -0,0 +1,16 @@
+hash_brown = {
+  "topping_1" => "Sour Cream",
+  "topping_2" => "Butter",
+  "topping_3" => "Salt",
+  "topping_4" => "Ketchup"
+}
+
+puts hash_brown["topping_3"]
+
+hash = Hash.new
+
+hash["name"] = "Theo"
+
+puts hash
+
+puts hash["name"]
