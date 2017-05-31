597ed2e9daefff2b713f9be1900aebe92862b4c9
Theo <theobaldo_medjunior@yahoo.com.br>
2017-05-30 21:10:49 -0300

    Class 6

---
+++ class_5.rb
@@ -0,0 +1,18 @@
+rock_bottom = "Mary Kate Lindsay Lohan Charlie Sheen"
+
+puts rock_bottom.split
+
+date = "05/30/2017"
+
+puts date.split("/")
+
+puts date[2]
+
+puts date[0..5]
+
+puts date[0...5]
+
+
+array_families = ["Brady", "Bundy"]
+
+puts array_families
---
+++ class_6.rb
@@ -0,0 +1,11 @@
+refris = ["coke", "sprite", "fanta"]
+
+puts refris[2]
+
+puts refris[-2]
+
+refris.each do |refri|
+  puts "ice #{refri}"
+end
+
+
